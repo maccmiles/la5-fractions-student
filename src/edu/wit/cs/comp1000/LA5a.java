894a79bfc45991d608b49cc3972c63ce48cfe658
Miles Macchiaroli <macchiarolim@wit.edu>
2017-11-09 18:13:21 -0500

    Written, Commented, Tested

--- src/edu/wit/cs/comp1000/LA5a.java
+++ src/edu/wit/cs/comp1000/LA5a.java
@@ -1,5 +1,5 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;//import scanner
 public class LA5a {
 	
 	/**
@@ -76,7 +76,11 @@ public class LA5a {
 	 * @return result of subtracting parameter f2 from f1 (numerator=[0], denominator=[1])
 	 */
 	public static int[] subtractFractions(int[] f1, int[] f2) {
-		return new int[2]; // TODO: replace with your code
+		int[] result = new int[2];
+		result[0] = (f1[0] * f2[1]) - (f2[0] * f1[1]);
+		result[1] = f1[1] * f2[1];
+		
+		return simplifyFraction(result);
 	}
 	
 	/**
@@ -87,7 +91,11 @@ public class LA5a {
 	 * @return result of multiplying parameters (numerator=[0], denominator=[1])
 	 */
 	public static int[] multiplyFractions(int[] f1, int[] f2) {
-		return new int[2]; // TODO: replace with your code
+		int[] result = new int[2];
+		result[0] = f1[0] * f2[0];
+		result[1] = f1[1] * f2[1];
+		
+		return simplifyFraction(result);
 	}
 	
 	/**
@@ -98,11 +106,56 @@ public class LA5a {
 	 * @return result of dividing parameter f2 by f1 (numerator=[0], denominator=[1])
 	 */
 	public static int[] divideFractions(int[] f1, int[] f2) {
-		return new int[2]; // TODO: replace with your code
+		int[] result = new int[2];
+		result[0] = f1[0] * f2[1];
+		result[1] = f1[1] * f2[0];
+		
+		return simplifyFraction(result);
 	}
 
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);//init scanner
+		//VARS
+		int[] f0 = new int[2];
+		int[] f1 = new int[2];
+		String sign = "";
+		int[] ans = new int[2];
+		/////
+		//IN
+		System.out.printf("Enter the numerator for the first fraction: ");
+		f0[0] = input.nextInt();
+		System.out.printf("Enter the denominator for the first fraction: ");
+		f0[1] = input.nextInt();
+		System.out.printf("Enter the numerator for the second fraction: ");
+		f1[0] = input.nextInt();
+		System.out.printf("Enter the denominator for the second fraction: ");
+		f1[1] = input.nextInt();
+		System.out.printf("Enter the operation (+, -, *, /): ");
+		sign = input.next();
+	     //////
+	//Check zero
+			if ((f0[1] == 0)||(f1[1] == 0)){System.out.printf("Denominator cannot be zero.%n");System.exit(0);}
+		//////
+		//Check Sign
+		if (sign.equals("+")){//Add
+			ans = addFractions(f0, f1);
+		}else if (sign.equals("-")){//Sub
+			ans = subtractFractions(f0, f1);
+		}else if (sign.equals("*")){//Mul
+			ans = multiplyFractions(f0, f1);
+		}else if (sign.equals("/")){//Div
+			if ((f0[1] == 0)||(f1[1] == 0)||(f1[0] == 0)){System.out.printf("Cannot divide by zero.%n");System.exit(0);}
+			ans = divideFractions(f0, f1);
+		}else {//Error in sign
+			System.out.printf("Invalid operation.%n");
+			System.exit(0);
+		}
+		if(ans[1] > 1) {//if denominator persists
+		System.out.printf("%d/%d %s %d/%d = %d/%d%n", f0[0], f0[1], sign, f1[0], f1[1], ans[0], ans[1]);
+		}else {
+		System.out.printf("%d/%d %s %d/%d = %d%n", f0[0], f0[1], sign, f1[0], f1[1], ans[0]);
+		}
+		
 	}
 
 }
